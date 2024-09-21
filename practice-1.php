4f234ef0291f6e9375e4b8d2e813252a215ccb5a
Noeliacj <n.casado.2022@alumnos.urjc.es>
2024-09-21 16:53:50 +0200

    Empezando web

---
+++ practice-1.php
@@ -0,0 +1,8 @@
+<?php require('./layout/header.php') ?>
+
+    <main>
+        <img src="img/vacuum-cleaner.jpg" alt="">
+        <h1>Practice 1 - Vacuum Cleaner</h1>
+    </main>
+
+<?php require('./layout/footer.php') ?>
---
+++ practice-2.php
@@ -0,0 +1,7 @@
+<?php require('./layout/header.php') ?>
+
+    <main>
+        <h1>Practice 2 - </h1>
+    </main>
+
+<?php require('./layout/footer.php') ?>
